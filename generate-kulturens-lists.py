1fe82e83e3bd14ffe5ef5bd2818e7d368b10da55
Emil Styrke <emil.styrke@gmail.com>
2013-06-24 21:27:59 +0200

    Generate complete attendance sheet.

--- generate-kulturens-lists.py
+++ generate-kulturens-lists.py
@@ -4,6 +4,9 @@ from config import *
 
 __author__ = 'emil'
 
+import csv
+import datetime
+
 from reportlab.platypus import PageTemplate, BaseDocTemplate, Frame
 from reportlab.platypus import NextPageTemplate, Paragraph, PageBreak
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
@@ -11,6 +14,7 @@ from reportlab.rl_config import defaultPageSize
 from reportlab.lib.units import inch
 from reportlab.pdfbase import pdfmetrics
 from reportlab.graphics import renderPDF
+from reportlab.pdfgen import canvas
 
 from pdfrw import PdfReader
 from pdfrw.buildxobj import pagexobj
@@ -20,19 +24,12 @@ PAGE_WIDTH = defaultPageSize[0]
 PAGE_HEIGHT = defaultPageSize[1]
 
 
-class MyTemplate(PageTemplate):
+class MyTemplate():
     """The kernel of this example, where we use pdfrw to fill in the
     background of a page before writing to it.  This could be used to fill
     in a water mark or similar."""
 
     def __init__(self, pdf_template_filename, name=None):
-        frames = [Frame(
-            0.85 * inch,
-            0.5 * inch,
-            PAGE_WIDTH - 1.15 * inch,
-            PAGE_HEIGHT - (1.5 * inch)
-            )]
-        PageTemplate.__init__(self, name, frames)
         # use first page as template
         page = PdfReader(pdf_template_filename).pages[0]
         self.page_template = pagexobj(page)
@@ -40,7 +37,7 @@ class MyTemplate(PageTemplate):
         self.page_xscale = PAGE_WIDTH/self.page_template.BBox[2]
         self.page_yscale = PAGE_HEIGHT/self.page_template.BBox[3]
 
-    def beforeDrawPage(self, canvas, doc):
+    def beforeDrawPage(self, canvas):
         """Draws the background before anything else"""
         canvas.saveState()
         rl_obj = makerl(canvas, self.page_template)
@@ -48,64 +45,129 @@ class MyTemplate(PageTemplate):
         canvas.doForm(rl_obj)
         canvas.restoreState()
 
-class MyDocTemplate(BaseDocTemplate):
-    """Used to apply heading to table of contents."""
+def drawPage(canvas, members, attendance):
+    canvas.saveState()
+    canvas.setFont("Helvetica", 10)
+    canvas.drawString(220, 765, CHOIR_NAME)
+    canvas.setFont("Times-Roman", 9)
+    canvas.drawString(357, 730, LEADER_NAME)
+    canvas.drawString(357, 712, LEADER_ADDRESS)
+    canvas.drawString(357, 703, LEADER_ADDRESS2)
 
-    def beforePage(self):
-        self.canv.saveState()
-        self.canv.setFont("Helvetica", 10)
-        self.canv.drawString(220, 765, CHOIR_NAME)
-        self.canv.setFont("Times-Roman", 9)
-        self.canv.drawString(357, 730, LEADER_NAME)
-        self.canv.drawString(357, 712, LEADER_ADDRESS)
-        self.canv.drawString(357, 703, LEADER_ADDRESS2)
+    canvas.setFont("Times-Italic", 8)
+    canvas.drawString(61, 663, START_DATE)
+    canvas.drawString(111, 663, MEETING_TIME)
+    canvas.drawString(181, 663, MEETING_DAY)
+    canvas.drawString(249, 663, CHOIR_NAME)
 
-        self.canv.setFont("Times-Italic", 8)
-        self.canv.drawString(61, 663, START_DATE)
-        self.canv.drawString(111, 663, MEETING_TIME)
-        self.canv.drawString(181, 663, MEETING_DAY)
-        self.canv.drawString(249, 663, CHOIR_NAME)
+    canvas.drawString(96, 640, MEETING_ADDRESS1)
+    canvas.drawString(96, 631, MEETING_ADDRESS2)
 
-        self.canv.drawString(96, 640, MEETING_ADDRESS1)
-        self.canv.drawString(96, 631, MEETING_ADDRESS2)
+    for i, e in enumerate(attendance.events):
+        addEvent(canvas, i, e)
 
-        addPerson(self.canv, 1, LEADER_NAME, LEADER_ADDRESS + ", " + LEADER_ADDRESS2, homePhone=LEADER_HOME_PHONE, mobilePhone=LEADER_MOBILE_PHONE, pnr=LEADER_PNR, bold=True)
-        addPerson(self.canv, 2, LEADER2_NAME, LEADER2_ADDRESS, mobilePhone=LEADER2_MOBILE_PHONE, pnr=LEADER2_PNR)
+    for i, m in enumerate(members):
+        addPerson(canvas, i, m, attendance.getAttendance(m.firstname, m.lastname))
 
-        self.canv.restoreState()
+    canvas.restoreState()
 
 
-def addPerson(canv, pos, name, address, **kwargs):
+def addPerson(canv, pos, member, attendance, **kwargs):
     canv.saveState()
 
-    y = 628 - (pos * 26)
+    y = 602 - (pos * 26.28)
 
-    if kwargs.get('bold', False):
+    if member.leader:
         canv.setFont("Times-Bold", 10)
     else:
         canv.setFont("Times-Roman", 10)
 
-    canv.drawString(96, y, name)
-    canv.drawString(96, y-9, address)
+    canv.drawString(96, y, member.firstname + " " + member.lastname)
 
-    if 'homePhone' in kwargs:
-        canv.drawString(268, y, "B %s" % kwargs['homePhone'])
+    addrLine = member.address1.decode("utf-8") + u", " + member.address2.decode("utf-8")
+    while canv.stringWidth(addrLine, "Times-Roman", 10) > 170:
+        addrLine = addrLine[:-1]
+    canv.drawString(96, y-10, addrLine)
 
-    if 'mobilePhone' in kwargs:
-        canv.drawString(268, y-9, "M %s" % kwargs['mobilePhone'])
+    if member.homePhone:
+        canv.drawString(268, y, "B %s" % filter(lambda c: c in "01234567890+-", member.homePhone))
 
-    if 'pnr' in kwargs:
+    if member.mobile:
+        canv.drawString(268, y-9, "M %s" % filter(lambda c: c in "01234567890+-", member.mobile))
+
+    if member.pnr:
         canv.setFont("Times-Roman", 7)
-        canv.drawString(52, y-11, kwargs['pnr'])
+        canv.drawString(52, y-11, member.pnr)
+
+    addAttendance(canv, y, attendance)
+    canv.restoreState()
+
+def addEvent(canv, pos, event):
+    canv.saveState()
+
+    x = 347 + pos * 11.3
+
+    canv.setFont("Helvetica", 7)
+
+    canv.drawString(x, 665, str(event[0].day))
+    canv.drawString(x, 642, str(event[0].month))
+    canv.drawString(x, 200, str(event[3]))
+    #canv.drawString(x, 175, "3")
+
+    if event[1] and event[1] != "19-21:30":
+        canv.drawString(x, 620, str(event[1]))
 
     canv.restoreState()
 
-def create_pdf(filename, pdf_template_filename):
+
+def addAttendance(canv, y, attendance):
+    canv.saveState()
+
+    for i, a in enumerate(attendance):
+        x = 349 + i * 11.3
+
+        canv.setFont("Helvetica", 13)
+        #canv.setFillColorRGB(0.2, 0.2, 0.2)
+
+        if a:
+            canv.drawString(x, y-3, "/")
+        else:
+            canv.drawString(x, y-3, "-")
+
+    canv.restoreState()
+
+
+def create_pdf(filename, pdf_template_filename, members, attendance):
     """Create the pdf, with all the contents"""
-    pdf_report = open(filename, "w")
-    document = MyDocTemplate(pdf_report)
-    templates = [MyTemplate(pdf_template_filename, name='background')]
-    document.addPageTemplates(templates)
+    pdf_report = canvas.Canvas(filename)
+    template = MyTemplate(pdf_template_filename)
+
+    members.sort(key=lambda x: not x.leader)
+
+    for a in attendance.attendees:
+        found = False
+        for m in members:
+            if a[0].strip() + " " + a[1].strip() == m.firstname.strip() + " " + m.lastname.strip():
+                found = True
+
+        if not found:
+            print "Member %s %s not found" % (a[0], a[1])
+
+    while True:
+        a1, a2 = attendance.split(20)
+
+        for page in range(0, len(members), 15):
+            template.beforeDrawPage(pdf_report)
+            drawPage(pdf_report, members[page:page+15], a1)
+            pdf_report.showPage()
+
+        if len(a2.events) == 0:
+            break
+
+        attendance = a2
+
+    pdf_report.save()
+    #document.addPageTemplates(templates)
 
     styles = getSampleStyleSheet()
     elements = [NextPageTemplate('background')]
@@ -114,11 +176,136 @@ def create_pdf(filename, pdf_template_filename):
     #for i in range(200):
     #    elements.append(Paragraph("Hello World" + str(i), styles['Heading1']))
 
-    document.multiBuild(elements)
-    pdf_report.close()
+    #document.multiBuild(elements)
+    #pdf_report.close()
+
+
+class Member(object):
+    def __init__(self, fields):
+        self.firstname = fields[9]
+        self.lastname = fields[6]
+        self.mobile = fields[25] or None
+        self.homePhone = fields[24] or None
+        self.address1 = ", ".join(filter(lambda x: len(x) > 0, fields[0:3]))
+        self.address2 = fields[16]
+        self.pnr = fields[15] or None
+        self.leader = self.firstname.strip() + " " + self.lastname.strip() in LEADERS
+
+
+def parse_matrikel(filename):
+    f = open(filename, 'rU')
+
+    pre_lines = f.readlines()[1:]
+    pre_lines.reverse()
+    lines = []
+    line = ""
+    for l in pre_lines:
+        line = l + line
+
+        d = line.split("\t")
+        if len(d) == 30:
+            lines.insert(0, line)
+            line = ""
+
+    if len(line) > 0:
+        lines.insert(0, line)
+
+    members = []
+    for l in lines:
+        d = l.split("\t")
+        member = Member(d)
+        members.append(member)
+        print member.__dict__
+
+    return members
+
+
+class Attendance(object):
+    def __init__(self):
+        self.events = []
+        self.attendees = []
+
+    def addEvent(self, date, time, comment, total):
+        if len(date) == 0:
+            return
+
+        self.events.append((datetime.datetime.strptime(date, "%Y-%m-%d"), time, comment, int(total)))
+
+    def addAttendee(self, firstname, lastname, comment, total, attendance):
+        attendance = [a == "1" for a in attendance]
+
+        if len(attendance) > len(self.events):
+            if sum(attendance[len(self.events):]) > 0:
+                raise RuntimeError("%s %s registered for %d of %d events" % (firstname, lastname, len(attendance), len(self.events)))
+            attendance = attendance[:len(self.events)]
+
+        if sum(attendance) != int(total):
+            raise RuntimeError("Attendance mismatch for %s %s: %d vs %d" % (firstname, lastname, sum(attendance), int(total)))
+        self.attendees.append((firstname, lastname, comment, int(total), attendance))
+
+    def getAttendance(self, firstname, lastname):
+        for i in self.attendees:
+            if i[0].strip() == firstname.strip() and i[1].strip() == lastname.strip():
+                return i[4]
+
+        print "Attendee %s %s not found" % (firstname, lastname)
+        return [False for i in range(len(self.events))]
+
+    def split(self, n):
+        a1 = Attendance()
+        a2 = Attendance()
+
+        a1.events = self.events[:n]
+        a2.events = self.events[n:]
+
+        for a in self.attendees:
+            attendance1 = a[4][:n]
+            attendance2 = a[4][n:]
+            a1.attendees.append((a[0], a[1], a[2], sum(attendance1), attendance1))
+            a2.attendees.append((a[0], a[1], a[2], sum(attendance2), attendance2))
+
+        a1.verify()
+        a2.verify()
+
+        return a1, a2
+
+    def verify(self):
+        for i, e in enumerate(self.events):
+            total = sum([a[4][i] for a in self.attendees])
+            if total != e[3]:
+                raise RuntimeError("Attendance mismatch for %s: %d vs %d", e[0], total, e[3])
+
+
+def parse_attendance(filename):
+    lines = []
+    with open(filename, 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in spamreader:
+            lines.append(row)
+
+    attendance = Attendance()
+    dates = lines[0][5:]
+    times = lines[1][5:]
+    comments = lines[2][5:]
+    total = lines[3][5:]
+
+    for date, time, comment, total in zip(dates, times, comments, total):
+        attendance.addEvent(date, time, comment, total)
+
+    for fields in lines[5:]:
+        if len(fields[0]) == 0:
+            continue
+
+        attendance.addAttendee(fields[0], fields[1], fields[3], fields[4], fields[5:])
+
+    attendance.verify()
+    return attendance
+
 
 def main():
-    create_pdf("hello.pdf", "narvarolista_tom.pdf")
+    matrikel = parse_matrikel("matrikel.txt")
+    attendance = parse_attendance("LKSS Närvaro 2013v - Närvaro.csv")
+    create_pdf("hello.pdf", "narvarolista_tom.pdf", matrikel, attendance)
 
 
 if __name__=="__main__":
